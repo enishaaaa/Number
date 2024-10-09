293e5bdde0dd1cc41436ee989fc75f364915260f
enishaaaa <enishaenisha2@gmail.com>
2024-10-09 10:11:02 +0530

    Number.java

--- Number.java
+++ Number.java
@@ -1,16 +1,11 @@
-public class Number
-public static void main(String args[])   
-{  
-int number=100;  
-System.out.print("List of odd numbers from 1 to "+number+": ");  
-for (int i=1; i<=number; i++)   
-{  
-//logic to check if the number is odd or not  
-//if i%2 is not equal to zero, the number is odd  
-if (i%2!=0)   
-{  
-System.out.print(i + " ");  
-}  
-}  
-}  
-}  
+class Number {
+   public static void main(String args[]) {
+	int n = 100;
+	System.out.print("Odd Numbers from 1 to "+n+" are: ");
+	for (int i = 1; i <= n; i++) {
+	   if (i % 2 != 0) {
+		System.out.print(i + " ");
+	   }
+	}
+   }
+}
